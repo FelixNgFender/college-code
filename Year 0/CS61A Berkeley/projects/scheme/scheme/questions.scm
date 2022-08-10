053541eca49d991d1d740f37192d10bba89be4d2
Thinh Nguyen <ngthinh302@gmail.com>
2022-08-10 13:30:30 +0700

    done scheme

---
+++ Year 0/CS61A Berkeley/projects/scheme/scheme/questions.scm
@@ -0,0 +1,83 @@
+(define (caar x) (car (car x)))
+(define (cadr x) (car (cdr x)))
+(define (cdar x) (cdr (car x)))
+(define (cddr x) (cdr (cdr x)))
+
+
+;; Problem 15
+;; Returns a list of two-element lists
+(define (enumerate s)
+; BEGIN PROBLEM 15
+  (define (helper index li)
+    (cond 
+      ((equal? li nil) nil)
+      (else 
+        (cons 
+          (list index (car li))
+          (helper (+ index 1) (cdr li))
+        )
+      )    
+    )
+  )
+  (helper 0 s)
+)
+; END PROBLEM 15
+
+;; Problem 16
+
+;; Merge two lists LiST1 and LiST2 according to COMP and return
+;; the merged lists.
+(define (merge comp list1 list2)
+  ; BEGIN PROBLEM 16
+  (cond
+    ((equal? list1 nil) list2)
+    ((equal? list2 nil) list1)
+    ((comp (car list1) (car list2)) 
+      (cons (car list1) (merge comp (cdr list1) list2))
+    )
+    (else 
+      (cons (car list2) (merge comp list1 (cdr list2))))
+  )
+)
+  ; END PROBLEM 16
+
+
+(merge < '(1 5 7 9) '(4 8 10))
+; expect (1 4 5 7 8 9 10)
+(merge > '(9 7 5 1) '(10 8 4 3))
+; expect (10 9 8 7 5 4 3 1)
+
+;; Problem 17
+
+(define (nondecreaselist s)
+; BEGIN PROBLEM 17
+  (define (helper li) ;返回的是一个list(a b c)
+    (cond ;这里没有检测nil
+      ((equal? (car li) nil) nil)
+      ((equal? (cdr li) nil) (cons (car li) nil))
+      (else
+        (cond 
+          ((> (car li) (cadr li)) (cons (car li) nil))
+          (else (cons (car li) (helper (cdr li)) ))
+        )
+      )
+    )
+  )
+  (define (remove li len)
+    (cond 
+      ((equal? len 0) li)
+      (else (remove (cdr li) (- len 1)))
+    )
+  )
+  (cond
+    ((equal? s nil) nil)
+    (else 
+      (define temp (helper s))
+      (define len (length temp))
+      (cons temp (nondecreaselist (remove s len)))
+    )
+  )
+)
+    ; END PROBLEM 17
+
+
